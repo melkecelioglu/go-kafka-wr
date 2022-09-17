9f199ffa761b4120a3f3f2fa970c3d1909d217cc
meltheMage <melkecelioglu@gmail.com>
2022-09-17 17:32:04 +0300

    4

---
+++ go.mod
@@ -0,0 +1,14 @@
+module awesomeProject4
+
+go 1.18
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/segmentio/kafka-go v0.4.34
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde
+)
+
+require (
+	github.com/klauspost/compress v1.15.7 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"awesomeProject4/kafka"
+	"context"
+	kafkago "github.com/segmentio/kafka-go"
+	"golang.org/x/sync/errgroup"
+	"log"
+)
+
+func main() {
+	reader := kafka.NewKafkaReader()
+	writer := kafka.NewKafkaWriter()
+
+	ctx := context.Background()
+	messages := make(chan kafkago.Message, 1000)
+	messageCommitChan := make(chan kafkago.Message, 1000)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		return reader.FetchMessage(ctx, messages)
+	})
+
+	g.Go(func() error {
+		return writer.WriteMessages(ctx, messages, messageCommitChan)
+	})
+
+	g.Go(func() error {
+		return reader.CommitMessages(ctx, messageCommitChan)
+	})
+
+	err := g.Wait()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
